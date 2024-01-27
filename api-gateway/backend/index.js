5491a7b04d5f8f26d7692c649445c95887ec155a
duc-hai <duchai621859@gmail.com>
2024-01-27 20:31:32 +0700

    build production enviroment with babel package

--- api-gateway/backend/index.js
+++ api-gateway/backend/index.js
@@ -8,8 +8,8 @@ const cookieParser = require('cookie-parser')
 env.config()
 database.connect()
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ limit: '50mb', extended: true}))
+app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
 const HOST = process.env.HOST || 'localhost'
