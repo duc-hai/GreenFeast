118a6a2b5c889af2627cf4d282b14c868db32a90
duc-hai <duchai621859@gmail.com>
2024-07-09 21:22:51 +0700

    Swap using redis online

--- api-gateway/backend/config/connect.redis.js
+++ api-gateway/backend/config/connect.redis.js
@@ -5,8 +5,13 @@ const redis = require('redis')
 async function connectRedis() {
     try {
         const client = await redis.createClient({
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT
+            // host: process.env.REDIS_HOST,
+            // port: process.env.REDIS_PORT
+            password: 'jsn0E8ZxYvzY1uP0eEs8vqYy4QnnIqEz',
+            socket: {
+                host: 'redis-14703.c12.us-east-1-4.ec2.redns.redis-cloud.com',
+                port: 14703
+            }
         })
 
         client.on('connect', () => {
