28da0e7c8b25377ba86dc796eb162a541bed4c26
Ngan Tram <tramngantruong.1007@gmail.com>
2024-09-13 10:24:08 +0700

    fix socketIO

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -2,9 +2,9 @@ import { createContext } from "react";
 import io from "socket.io-client";
 import Cookies from "js-cookie";
 // export const SOCKET_URL = `${process.env.REACT_APP_API}`;
-export const SOCKET_URL = "https://greenfeast.space";
+export const SOCKET_URL = "";
 const token = Cookies.get("accessToken");
-const socket = io(`https://greenfeast.space/notification`, {
+const socket = io(`https://greenfeast.space/notificationssss`, {
   auth: {
     token: token,
   },
@@ -18,5 +18,4 @@ const SocketProvider = ({ children }) => {
     </SocketContext.Provider>
   );
 };
-
 export default SocketProvider;
