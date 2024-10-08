3a6322bbef69d2e7a07c06779d10742ec62180f8
duc-hai <duchai621859@gmail.com>
2024-10-08 21:30:03 +0700

    Retrain machine learning models testing auto

--- order-service/backend/index.js
+++ order-service/backend/index.js
@@ -27,6 +27,7 @@ app.use('/', routes)
 cron.schedule('0 0 * * *', syncRecommendService.mainSyncRecommend) // Retrain machine learning models at 0AM
 cron.schedule('0 15 * * *', syncRecommendService.mainSyncRecommend) // Retrain machine learning models at 3PM
 cron.schedule('30 21 * * *', syncRecommendService.mainSyncRecommend) // Testing
+cron.schedule('0 22 * * *', syncRecommendService.mainSyncRecommend) // Testing
 
 app.listen(PORT, () => {
     console.log(`Order service is running at http://${HOST}:${PORT}`)
